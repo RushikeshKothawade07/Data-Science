b0a55cbc248c054db2bc710d7d3b8975b35b4503
Rushikesh Kothawade <97045515+RushikeshKothawade07@users.noreply.github.com>
2023-06-15 20:00:43 +0530

    Add files via upload

---
+++ cv/HandTracking/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+    # cv2.rectangle(img,(50,int(volBar)),(85,400),(62,255,0),cv2.FILLED)
+    # cv2.putText(img,f'{int(volPerc)} %',(40,450),cv2.FONT_HERSHEY_PLAIN,2,(0,200,0),3)    
+     
